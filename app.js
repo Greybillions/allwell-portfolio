27dc2c5eb5c4098cfb2b9b9c64e2052a790b569c
Graham Boyle <olayile1999@gmail.com>
2024-05-27 00:54:06 +0100

    updated

--- app.js
+++ app.js
@@ -39,3 +39,20 @@ window.addEventListener('scroll', () => {
     }
   });
 });
+
+// initialize swiper js
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
